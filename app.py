97931d10b594df1466d2636ee114182c4ef84a79
Peter Davison <peterd@spaceflightindustries.com>
2020-03-22 22:33:12 -0400

    Add game/player ID to cookies and check for validity at endpoint

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-from flask import Flask, render_template, url_for, redirect, request
+import string
+import random
+
+from flask import Flask, render_template, url_for, redirect, request, make_response
+
+
 
 gpath = "data/games.txt"
 ppath = "data/players.txt"
@@ -34,13 +39,18 @@ def add_game():
 @app.route("/joingame/")
 def join_game():
     gid = request.args.get("gid", None)
-    if gid and check_game(gid):
-        return redirect(url_for("new_player"))
+    if gid and check_game_id(gid):
+        resp = make_response(redirect(url_for("new_player")))
+        resp.set_cookie("gid", gid)
+        return resp
     else:
         return redirect(url_for("bad"))
 
 
-def check_game(gid):
+def check_game_id(gid):
+    """
+    Check game ID string against db
+    """
     with open(gpath, "r") as fptr:
         games = fptr.read().split()
 
@@ -50,28 +60,87 @@ def check_game(gid):
         return False
 
 
+def check_player_id(pid):
+    """
+    Check player ID string against db
+    """
+    with open(ppath, "r") as fptr:
+        players = fptr.read().split()
+
+    player_ids = [s.split(":")[0] for s in players]
+
+    if pid in player_ids:
+        return True
+    else:
+        return False
+
+
+def create_rand_id(n=10):
+    chars = string.ascii_uppercase
+    return "".join(random.choice(chars) for _ in range(n))
+
+
+def auth_game(req):
+    """
+    Check cookies for game ID
+    """
+    gid = req.cookies.get("gid", None)
+    if gid:
+        return check_game_id(gid)
+    else:
+        return False
+
+
+def auth_player(req):
+    """
+    Check cookies for valid player ID
+    """
+    pid = req.cookies.get("pid", None)
+    if pid:
+        return check_player_id(pid)
+    else:
+        return False
+
+
 @app.route("/newplayer/")
 def new_player():
-    # auth the game id
+    # maybe check to make sure a player doesn't already exist in cookies to stop one user creating multiple players
+    if not auth_game(request):
+        return redirect(url_for("bad"))
     return render_template("newplayer.html")
 
 
 @app.route("/submitplayer/")
 def submit_player():
-    # do some stuff to create the player
+    if not auth_game(request):
+        return redirect(url_for("bad"))
+
+    pname = request.args.get("pname", None)
+    if not pname:
+        return redirect(url_for("submit_player"))
+
+    pid = create_rand_id()
+
     with open(ppath, "a") as fptr:
-        fptr.write(f"{request.args.get('pname', 'failed')}\n")
-    return redirect(url_for("new_words"))
+        fptr.write(f"{pid}:{pname}\n")
+
+    resp = make_response(redirect(url_for("new_words")))
+    resp.set_cookie("pid", pid)
+    return resp
 
 
 @app.route("/newwords/")
 def new_words():
+    if not auth_player(request):
+        return redirect(url_for("bad"))
     return render_template("newwords.html")
 
 
 @app.route("/submitwords/")
 def submit_words():
-    # do some stuff to store words
+    if not auth_player(request):
+        return redirect(url_for("bad"))
+    
     with open(wpath, "a") as fptr:
         fptr.write(f"{request.args.get('word1', 'failed')}\n")
         fptr.write(f"{request.args.get('word2', 'failed')}\n")
