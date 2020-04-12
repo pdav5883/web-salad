bd14f272c81ddf5bbb9b8fb658bcd86c399786d4
Peter <davison.pl@gmail.com>
2020-04-12 14:57:08 -0400

    Remove old data files, ignore future data changes

--- data/reset.sh
+++
@@ -1,6 +0,0 @@
-dir=$1
-rm $dir/games.txt $dir/players.txt $dir/words.txt
-echo -e '{"abcasd": {"num_words": "5", "t1": "60", "t2": "60", "t3": "30", "started": false, "complete": false},' > $dir/games.txt
-echo -e '"wxyz": {"num_words": "3", "t1": "7", "t2": "7", "t3": "7", "started": false, "complete": false}}\n' >> $dir/games.txt
-echo "{}" > $dir/players.txt
-echo "{}" > $dir/words.txt
